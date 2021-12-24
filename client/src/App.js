74ff0d898853af47fc3913bdfa792d5ef346c831
laca8 <ahmedragab01140331005@gmail.com>
2021-12-24 16:50:02 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React,{useState} from "react"
+const api = {
+  key:'e3587ac375d0ce8a812ee700f29c7983',
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+function App() {
+  const [query,setQuery] = useState('')
+  const [weather,setWeather] = useState('')
+  const dateBuilder = (d) =>{
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = [ "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate()
+    let month = months[d.getMonth()];
+    let year = d.getFullYear()
+    return `${day} ${date} ${month} ${year}`
+  }
+  const search = (e)=>{
+    if(e.key === 'Enter'){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('')
+        console.log(result);
+      
+    })
+    }
+  }
+  return (
+    <div className={
+      weather.main ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'
+    }>
+      <main>
+        <div className='search-box'>
+          <input type='text' className='search-bar' placeholder='Search...' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
+        </div>
+        {(typeof weather.main != 'undifined') ? (
+        <div>
+              <div className='location-box'>
+              <div className='location'>{weather.name} , {weather?.sys?.country}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+            </div>
+            <div className='weather-box'>
+              <div className='temp'>
+              {Math.round(weather.main? weather.main.temp : (''))}°C
+              </div>
+              <div className='weather'>
+                {weather.weather ? weather.weather[0].main : ('')}
+              </div>
+            </div>
+            </div>
+         ): ('')}
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
